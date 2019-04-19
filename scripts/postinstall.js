804290070e74601da590b6e5f5236aa81d941852
Larry Kirschner <larrykirschner@gmail.com>
2019-04-18 17:06:13 -0700

    init

---
+++ scripts/postinstall.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fs = require('fs-extra')
+const appRoot = require('app-root-path').path
+
+const installCmi5Lib = async() => {
+	const srcPath = path.join(
+		appRoot, 'node_modules', 'react-cmi5', 'src', 'cmi5.js'
+	)
+
+	try {
+		if(!await fs.exists(srcPath)) {
+			console.warn(`react-cmi5 should install client lib 'cmi5.js' to public in the react project where it is installed, but library not found at ${srcPath}`)
+			// we probably are inside the react-cmi project itself and did `npm install`
+			// there's no reason to do this library install in that context.
+			// The library install is for clients/projects that *use* react-cmi5
+			return
+		}
+		const tgtPath = path.join(appRoot, 'public', 'cmi5.js')
+		console.log(`react-cmi5 will attempt to install client lib from ${srcPath} to ${tgtPath}...`)
+		await fs.ensureDir(path.dirname(tgtPath))
+		await fs.copyFile(srcPath, tgtPath)
+	}
+	catch(err) {
+		console.error(`failed to install cmi5.js lib with error ${err.message}`)
+	}
+}
+
+installCmi5Lib()
