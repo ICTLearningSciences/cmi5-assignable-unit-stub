663f0bc960d7adfd1fba8280ff839bfdf50b2182
Larry Kirschner <larrykirschner@gmail.com>
2019-07-23 14:19:31 -0700

    makes calls to action 'completed' also terminate cmi5 session by default

--- src/actions.js
+++ src/actions.js
@@ -11,6 +11,12 @@ export const TERMINATE_SUCCEEDED = "CMI5_TERMINATE_SUCCEEDED"
 export const TERMINATE_FAILED = "CMI5_TERMINATE_FAILED"
 
 /**
+ * Mark the lesson as completed with a score (or no score if non assement), 
+ * and then (by default) terminate the cmi5 session.
+ * 
+ * Generally safer to use the combined 'complete then terminate'
+ * to more safely manage that the two events are published correctly in order.
+ * 
  * In CMI5 protocol, one of complete/pass/failed should be called once (and only once).
  * This single 'completed' function will send a result with a completion verb as follows:
  *   - COMPLETED (https://github.com/AICC/CMI-5_Spec_Current/blob/quartz/cmi5_spec.md#verbs_completed)
@@ -20,63 +26,87 @@ export const TERMINATE_FAILED = "CMI5_TERMINATE_FAILED"
  *   - FAILED (https://github.com/AICC/CMI-5_Spec_Current/blob/quartz/cmi5_spec.md#verbs_failed)
  *       If a `score` is passed and `failed` is `true`
  * (COMPLETED, PASSED, FAILED)
- * 
+ *
  * @params {Number} [score] - the score for PASSED or FAILED or leave undefined for non-assessment resources
  * @params {Boolean} [failed] - pass `true` *only* with a failing score
- * @params {Object} [extensions] - a XAPI extensions object to pass with result 
+ * @params {Object} [extensions] - a XAPI extensions object to pass with result
  *     (https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md#result)
+ * @params {Boolean} terminate - if TRUE, terminates the cmi5 session after
+ *      submitting 'completed'
  */
-export const completed = (score, failed, extensions) => (dispatch, getState) => {
-    const cmiStatus = Cmi5.getStatus(getState())
-    if(cmiStatus !== Cmi5.STATUS.STARTED && cmiStatus !== Cmi5.STATUS.COMPLETE_FAILED) {
-        console.error('complete called from invalid state (you need to call start action before complete and complete can be called only ONE time)', cmiStatus)
-        return
+export const completed = (score, failed, extensions, terminate = true) => (
+    dispatch,
+    getState
+  ) => {
+    const cmiStatus = Cmi5.getStatus(getState());
+    if (
+      cmiStatus !== Cmi5.STATUS.STARTED &&
+      cmiStatus !== Cmi5.STATUS.COMPLETE_FAILED
+    ) {
+      console.error(
+        'complete called from invalid state (you need to call start action before complete and complete can be called only ONE time)',
+        cmiStatus
+      );
+      return;
     }
-    const cmi = Cmi5.instance
-    if(!cmi) {
-        /**
-         * the cmi instance will be null if initialization failed with an error
-         * e.g. because this web-app was launched using a url 
-         * that didn't have cmi5's expected query params:
-         * https://github.com/AICC/CMI-5_Spec_Current/blob/quartz/cmi5_spec.md#content_launch
-         */
-        console.error('complete called having no cmi instance (you need to call start action before complete)')
-        return
+    const cmi = Cmi5.instance;
+    if (!cmi) {
+      /**
+       * the cmi instance will be null if initialization failed with an error
+       * e.g. because this web-app was launched using a url
+       * that didn't have cmi5's expected query params:
+       * https://github.com/AICC/CMI-5_Spec_Current/blob/quartz/cmi5_spec.md#content_launch
+       */
+      console.error(
+        'complete called having no cmi instance (you need to call start action before complete)'
+      );
+      return;
     }
-    dispatch({type: COMPLETE_REQUESTED})
-    const onDone = (err) => {
-        if(err) {
-            console.error('completion call failed with error:', err)
-            dispatch({type: COMPLETE_FAILED, error: err.message})
-            return
+    dispatch({ type: COMPLETE_REQUESTED });
+    const onCompleteCallback = err => {
+      if (err) {
+        console.error('completion call failed with error:', err);
+        dispatch({ type: COMPLETE_FAILED, error: err.message });
+      } else {
+        dispatch({ type: COMPLETE_SUCCEEDED });
+      }
+      if (!terminate) {
+        return;
+      }
+      dispatch({ type: TERMINATE_REQUESTED });
+      cmi.terminate(err => {
+        if (err) {
+          console.error('completion call failed with error:', err);
+          dispatch({ type: TERMINATE_FAILED, error: err.message });
+          return;
         }
-        dispatch({type: COMPLETE_SUCCEEDED})
-    }
-    if(isNaN(Number(score))) {
-        /**
-         * if no score is passed, then we will complete with 
-         * the COMPLETED verb (as opposed to PASSED or FAILED)
-         * https://github.com/AICC/CMI-5_Spec_Current/blob/quartz/cmi5_spec.md#verbs_completed
-         */
-        extensions = score
-        cmi.completed(extensions, onDone)
-        return
+        dispatch({ type: TERMINATE_SUCCEEDED });
+      });
+    };
+    if (isNaN(Number(score))) {
+      /**
+       * if no score is passed, then we will complete with
+       * the COMPLETED verb (as opposed to PASSED or FAILED)
+       * https://github.com/AICC/CMI-5_Spec_Current/blob/quartz/cmi5_spec.md#verbs_completed
+       */
+      extensions = score;
+      cmi.completed(extensions, onCompleteCallback);
+      return;
     }
     /**
-     * A score was passed, so we will complete with 
-     * the either verb PASSED 
+     * A score was passed, so we will complete with
+     * the either verb PASSED
      * (https://github.com/AICC/CMI-5_Spec_Current/blob/quartz/cmi5_spec.md#verbs_passed)
      * or FAILED
      * https://github.com/AICC/CMI-5_Spec_Current/blob/quartz/cmi5_spec.md#verbs_failed)
      */
-    failed = typeof(failed) === 'boolean'? failed: false
-    if(failed) {
-        cmi.failed(score, extensions, onDone)
+    failed = typeof failed === 'boolean' ? failed : false;
+    if (failed) {
+      cmi.failed(score, extensions, onCompleteCallback);
+    } else {
+      cmi.passed(score, extensions, onCompleteCallback);
     }
-    else {
-        cmi.passed(score, extensions, onDone)
-    }
-}
+  };
 
 /**
  * As early  as possible you must initialize cmi5 by calling the start action.
@@ -110,8 +140,7 @@ export const start = url => dispatch => {
 export const terminate = () => (dispatch, getState) => {
     const cmiStatus = Cmi5.getStatus(getState())
     if(cmiStatus !== Cmi5.STATUS.COMPLETED && cmiStatus !== Cmi5.STATUS.TERMINATE_FAILED) {
-        console.error('terminate called from invalid state', cmiStatus)
-        return
+        console.error('Terminate called when status is not COMPLETED. Generally safer to use "completeAndTerminate" action', cmiStatus)
     }
     const cmi = Cmi5.instance
     if(!cmi) {
